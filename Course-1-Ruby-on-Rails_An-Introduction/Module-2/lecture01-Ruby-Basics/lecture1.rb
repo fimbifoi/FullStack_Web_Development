275c912c2cb4106fb723a5be43668c694d521589
Fimbi Foi <fimbi3rd@gmail.com>
2018-12-21 08:58:30 +0100

    Changes to be committed: 	new file:   Course-1-Ruby-on-Rails_An-Introduction/Module-2/lecture01-Ruby-Basics/lecture1.rb 	completed lecture one code in the lecture

---
+++ Course-1-Ruby-on-Rails_An-Introduction/Module-2/lecture01-Ruby-Basics/lecture1.rb
@@ -0,0 +1,11 @@
+3.times {puts "Hello World"}
+
+#this is a comment
+puts 5 #, so is this
+3 # and this too
+
+p "Got it" #=> got it
+
+a=3 # semicolons are not obligatory
+
+a = 2; b =3 #sometimes could be used when variables are on the same line
